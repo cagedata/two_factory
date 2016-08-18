3b1c89c1edd71dd5ee47d8a32f0468f857535323
Dave Long <dlong@cagedata.com>
2016-08-17 23:03:01 -0400

    Added output of running port

--- lib/two_factor_app.ex
+++ lib/two_factor_app.ex
@@ -12,6 +12,7 @@ defmodule TwoFactorApp do
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: TwoFactorApp.Supervisor]
+    IO.puts "Running on Port #{Application.get_env(:two_factor_app, :port)}"
     Supervisor.start_link(children, opts)
   end
 end
